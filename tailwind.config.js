4d95f8d1dbd094679830d2b77fd2de1e7261da7b
Webster Elumelu <83818549+elumeluwebster@users.noreply.github.com>
2025-09-11 23:48:53 +0100

    icons and fonts

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        newsreader: ['Newsreader', 'serif'],
+        geist: ['Geist', 'sans-serif'],
+        'geist-mono': ['Geist Mono', 'monospace'],
+      },
+    },
+  },
+  plugins: [],
+};
