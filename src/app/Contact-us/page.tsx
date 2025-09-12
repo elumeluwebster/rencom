6e5697f0eac97d8825d0c3f1883fc44130578231
Webster Elumelu <83818549+elumeluwebster@users.noreply.github.com>
2025-09-12 05:12:20 +0100

    contact us page complete

--- src/app/Contact-us/page.tsx
+++ src/app/Contact-us/page.tsx
@@ -6,21 +6,21 @@ export default function Contact() {
     <>
       <HeaderDark />
       <div>
-        <section className="h-auto mt-24 mb-28 px-4 md:px-28 pb-8">
-          <div className="flex flex-col md:flex-row gap-16">
+        <section className="relative h-auto mt-20 mb-24 px-4 sm:px-6 md:px-28 pb-40">
+          <div className="flex flex-col md:flex-row gap-12 md:gap-16">
             <div className="flex-1">
-              <h2 className="text-3xl md:text-4xl font-regular text-[#0F3D1C] leading-snug">
+              <h2 className="text-2xl sm:text-3xl md:text-4xl font-regular text-[#0F3D1C] leading-snug">
                 Get Involved - <br /> Let's Build Together
               </h2>
             </div>
 
             <div className="flex-1">
-              <div className="bg-[#F5F5F5] p-6 rounded-2xl shadow-md">
-                <h3 className="text-2xl font-regular text-[#0F3D1C] mb-8">
+              <div className="bg-[#F5F5F5] p-6 sm:p-8 rounded-2xl shadow-md">
+                <h3 className="text-xl sm:text-2xl font-regular text-[#0F3D1C] mb-6 sm:mb-8">
                   Partner With Us
                 </h3>
-                <form className="space-y-8">
-                  <div className="flex flex-col mb-8 md:flex-row gap-4">
+                <form className="space-y-6 sm:space-y-8">
+                  <div className="flex flex-col sm:flex-row gap-4">
                     <input
                       type="text"
                       placeholder="First Name"
@@ -32,19 +32,22 @@ export default function Contact() {
                       className="flex-1 p-3 border border-[#EDEDED] rounded text-black"
                     />
                   </div>
+
                   <input
                     type="email"
                     placeholder="Email Address"
                     className="w-full p-3 border border-[#EDEDED] rounded text-black"
                   />
+
                   <textarea
                     placeholder="Message"
                     rows={5}
                     className="w-full p-3 border border-[#EDEDED] rounded text-black resize-none"
                   ></textarea>
+
                   <button
                     type="submit"
-                    className="bg-[#387F1A] text-white px-6 py-3 rounded-full w-full md:w-auto"
+                    className="bg-[#387F1A] cursor-pointer text-white px-6 py-3 rounded-full w-full sm:w-auto"
                   >
                     Contact Us →
                   </button>
@@ -52,21 +55,27 @@ export default function Contact() {
               </div>
             </div>
           </div>
+
+          <img
+            src="/assets/images/topographic.png"
+            alt=""
+            className="hidden md:block absolute left-0 -bottom-24 pointer-events-none select-none w-[400px] h-auto"
+          />
         </section>
 
-        <section className="bg-[#D8E4CA] w-full py-22 px-4 md:px-28">
+        <section className="bg-[#D8E4CA] w-full py-16 px-4 sm:px-6 md:px-28">
           <div className="flex flex-col md:flex-row gap-12 md:gap-20">
             <div className="flex-1">
-              <p className="text-[#0F3D1C] text-lg md:text-xl font-base">
+              <p className="text-[#0F3D1C] text-lg sm:text-xl font-base">
                 Contact Us
               </p>
-              <p className="text-[#0F3D1C] mt-4 text-4xl md:text-5xl font-regular leading-snug">
+              <p className="text-[#0F3D1C] mt-4 text-3xl sm:text-4xl md:text-5xl font-regular leading-snug">
                 Get In Touch <br /> with Us
               </p>
             </div>
 
             <div className="flex-1 flex flex-col gap-2">
-              <p className="text-[#000] text-lg md:text-[22px] font-semibold">
+              <p className="text-[#000] text-lg sm:text-xl font-semibold">
                 Email
               </p>
               <div className="w-8 h-1 bg-[#000]"></div>
@@ -80,7 +89,7 @@ export default function Contact() {
             </div>
 
             <div className="flex-1 flex flex-col gap-2">
-              <p className="text-[#000] text-lg md:text-[22px] font-semibold">
+              <p className="text-[#000] text-lg sm:text-xl font-semibold">
                 Phone
               </p>
               <div className="w-8 h-1 bg-[#000]"></div>
